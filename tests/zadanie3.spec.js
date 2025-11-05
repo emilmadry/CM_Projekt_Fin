1d2e59bd69e754acb91b6d9173404f2c209a6402
emilmadry <redvarg91@gmail.com>
2025-11-05 18:42:11 +0100

    test: prepare zad 3

---
+++ tests/zadanie3.spec.js
@@ -0,0 +1,13 @@
+// @ts-nocheck
+import { test, expect } from '@playwright/test';
+import { MainPage } from '../pages/main-page';
+
+
+test.skip('login as admin', async ({ page }) => {
+  // await page.getByTestId('login-username').fill(process.env.ADMIN_LOGIN);
+  // await page.getByTestId('login-password').fill(process.env.ADMIN_PASSWORD);
+  // await page.getByTestId('login-button').click();
+  // await expect(page.getByTestId('welcome-msg')).toContainText(`Witaj: ${process.env.ADMIN_LOGIN}`)
+});
+
+
